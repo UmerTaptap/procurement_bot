551fcba5749466b5171f71ba769974679af47a13
Umer <umer.iftikhar@taptaptechnologies.com>
2024-09-18 20:20:24 +0500

    init

--- start_services.sh
+++ start_services.sh
@@ -1,6 +1,6 @@
 cd app/
 # Start Rasa server with NLU model
-rasa run --model models --enable-api --cors "*" --debug -p $PORT &
+rasa run --endpoints endpoints.yml --enable-api --cors "*" --debug -p $PORT &
 
 # Optional: Start the Rasa action server if you have custom actions
 rasa run actions --debug &
