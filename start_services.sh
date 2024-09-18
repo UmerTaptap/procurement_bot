94865289708003de44707dde74cad094ab4256ea
Umer <umer.iftikhar@taptaptechnologies.com>
2024-09-18 21:18:40 +0500

    init

--- start_services.sh
+++ start_services.sh
@@ -1,7 +1,12 @@
+# cd app/
+
+# # Start Rasa server with NLU model
+# rasa run --model models --enable-api --cors “*” --debug -p $PORT &
+
+# # Optional: Start the Rasa action server if you have custom actions
+# rasa run actions --port 5000 --debug &
+
 cd app/
-
-# Start Rasa server with NLU model
-rasa run --model models --enable-api --cors “*” --debug -p $PORT &
-
-# Optional: Start the Rasa action server if you have custom actions
-rasa run actions --port 5000 --debug &
+# Start rasa server with nlu model
+rasa run --model models --enable-api --cors "*" --debug \
+         -p $PORT
